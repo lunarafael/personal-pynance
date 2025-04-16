4116e0da02f1ff14dd6c8737cb0cba11c13f1bea
Rafael Luna <rafampluna@hotmail.com>
2025-04-16 18:06:24 -0300

    Refactor category test to use fixture for category creation

--- backend/finance/tests/test_categories.py
+++ backend/finance/tests/test_categories.py
@@ -2,17 +2,26 @@ import pytest
 from rest_framework.test import APIClient
 from django.contrib.auth import get_user_model
 from finance.models import Category
+import uuid
+
+@pytest.fixture
+def create_categories():
+    category1 = Category.objects.create(name=f"Categoria-{uuid.uuid4()}")
+    category2 = Category.objects.create(name=f"Categoria-{uuid.uuid4()}")
+    return [category1, category2]
 
 @pytest.mark.django_db
-def test_list_categories():
+def test_list_categories(create_categories):
     user_model = get_user_model()
-    user = user_model.objects.create_user(username="testuser", password="1234")  # Cria um usuário
+    user = user_model.objects.create_user(username="testuser", password="1234")
+    
     client = APIClient()
-    client.force_authenticate(user=user)  # Autentica o cliente
-
-    Category.objects.create(name="Educação")
-    Category.objects.create(name="Saúde")
+    client.force_authenticate(user=user)
 
     response = client.get("/api/finance/categories/")
+
     assert response.status_code == 200
 
+    category_names = [category["name"] for category in response.data]
+    for category in create_categories:
+        assert category.name in category_names

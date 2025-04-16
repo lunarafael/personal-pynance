22a8a154141bd218c8efc6496a1748c42734eee7
Rafael Luna <82469173+lunarafael@users.noreply.github.com>
2025-04-16 17:00:51 -0300

    Feature/jwt (#2)
    * Add JWT permission classes
    
    * Add JWT authentication settings to REST framework
    
    * Add JWT token endpoints to URL configuration

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Category, Transaction
 from .serializers import CategorySerializer, TransactionSerializer
 
 class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TransactionListCreateView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,8 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]

f047c622f6523a5a221a6e008ddc4b39fb8064a2
Rafael Luna <kenkorio23@gmail.com>
2025-04-17 13:06:59 -0300

    Add IsStaffUser permission class and update UserListView permissions

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics, permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from .permissions import IsStaffUser
 
-class UserListCreateView(generics.ListCreateAPIView):
+class RegisterUserView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+
+class UserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated, IsStaffUser]

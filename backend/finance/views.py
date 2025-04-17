b0ad58f5cf687b0ae483711e937c69821e4ef0ce
Rafael Luna <kenkorio23@gmail.com>
2025-04-17 12:30:25 -0300

    Refactor Category and Transaction views to improve queryset handling and ensure user-specific data retrieval

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -2,17 +2,25 @@ from rest_framework import generics, permissions
 from .models import Category, Transaction
 from .serializers import CategorySerializer, TransactionSerializer
 
-class CategoryListCreateView(generics.ListCreateAPIView):
+class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
 
 class TransactionListCreateView(generics.ListCreateAPIView):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)

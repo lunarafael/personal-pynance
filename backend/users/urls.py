eb556829236a20a0a7eb491d69095cdbdf45f485
Rafael Luna <kenkorio23@gmail.com>
2025-04-17 13:07:08 -0300

    Update user URLs to correct view references and improve endpoint structure

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserListCreateView
+from .views import RegisterUserView, UserListView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user-list-create'),
+    path('register/', RegisterUserView.as_view(), name='user-create'), # POST /api/users/register/
+    path("list/", UserListView.as_view(), name="user-list") # GET /api/users/list/
 ]
